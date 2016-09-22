67068a2baad54de5d745a89facae437f97c8a2b4
Andrii Rozumnyi <Andrii.Rozumnyi@funkwerk-itk.com>
2016-09-22 10:28:31 +0300

    add basic parsing of iso duration

--- lib/logstash/filters/duration.rb
+++ lib/logstash/filters/duration.rb
@@ -16,9 +16,20 @@ class LogStash::Filters::Duration < LogStash::Filters::Base
   def filter(event)
 
     if @iso
-      # TODO parse duration in ISO format
+      days = @iso[/([0-9]+)D/, 1].to_i
+      hours = @iso[/([0-9]+)H/, 1].to_i
+      minutes = @iso[/([0-9]+)M/, 1].to_i
+      seconds = @iso[/([0-9]+)S/, 1].to_i
+      duration = total_seconds(days, hours, minutes, seconds)
+      event.set("duration", duration)
     end
 
     filter_matched(event)
   end # def filter
+
+  private
+  def total_seconds(days=0, hours=0, minutes=0, seconds=0)
+    days*86400 + hours*3600 + minutes*60 + seconds
+  end # def total_seconds
+
 end # class LogStash::Filters::Duration
